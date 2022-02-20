b20bc00721b2c48d50ad04264ee121231cbcc43d
Bidulman <63709148+Bidulman@users.noreply.github.com>
2022-02-20 20:29:27 +0100

    Added night vision for spectators

--- src/main/java/eu/bidulaxstudio/spectathor/SpectaThor.java
+++ src/main/java/eu/bidulaxstudio/spectathor/SpectaThor.java
@@ -9,6 +9,8 @@ import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.*;
 
@@ -59,10 +61,22 @@ public class SpectaThor extends JavaPlugin {
     public boolean spyPlayer(Player player) {
         if (player.getGameMode() != GameMode.SPECTATOR) {
             player.setGameMode(GameMode.SPECTATOR);
+
             savePosition(player);
+
+            if (getConfig().getBoolean("settings.nightVision.enabled")) {
+                player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 1000000, 255, false, false));
+            }
+
             return true;
+
         } else {
             player.setGameMode(getServer().getDefaultGameMode());
+
+            if (getConfig().getBoolean("settings.nightVision.enabled")) {
+                player.removePotionEffect(PotionEffectType.NIGHT_VISION);
+            }
+
             return false;
         }
     }
